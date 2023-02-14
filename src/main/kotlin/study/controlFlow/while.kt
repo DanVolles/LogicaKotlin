7d74020da1a170679c56d147657109a1b1a43014
Danilo Volles <danilo.volles@outlook.com>
2023-02-14 15:59:38 -0300

    New "(do)while" examples

--- src/main/kotlin/study/controlFlow/while.kt
+++ src/main/kotlin/study/controlFlow/while.kt
@@ -3,19 +3,24 @@ package study.controlFlow
 fun main(args: Array<String>) {
 
 // ----------- While
-    var sum = 1
-    while (sum < 1_000) {
-        sum += 1
-        println(sum)
-        break
+// O while sempre espera um booleano na sua condição. Enquanto true/false faça...
+    var i = 0
+    while (i <=    100) {
+        println(i)
+        i++
     }
 
 // Estrutura de repetição
-    var numeros = arrayOf(1, 2, 3, 4, 5, 6, 100, 200, 300, 400, 500, 600)
-    var i = 0
+    val numeros = arrayOf(1, 2, 3, 4, 5, 6, 100, 200, 300, 400, 500, 600)
+    var j = 0
     while (i <= numeros.size - 1) { // usa-se -1 por que o treco é estranho e adiciona um no array
         println(numeros[i])
         i++
     }
+
+    do {
+        print("Qaul o seu nome?")
+        val nome = readln()
+    } while (nome == "" || nome != "Danilo")
 
 }
