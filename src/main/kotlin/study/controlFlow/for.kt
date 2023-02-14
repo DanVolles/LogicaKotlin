0ffde70729c42beae543c183ae0eb322dfc39c20
Danilo Volles <danilo.volles@outlook.com>
2023-02-14 14:56:13 -0300

    New "for" examples

--- src/main/kotlin/study/controlFlow/for.kt
+++ src/main/kotlin/study/controlFlow/for.kt
@@ -2,10 +2,37 @@ package study.controlFlow
 
 fun main(args: Array<String>) {
 
-    var numeros = arrayOf(1, 2, 3, 4, 5, 6, 100, 200, 300, 400, 500, 600)
+    val numeros = arrayOf(1, 2, 3, 4, 5, 6, 100, 200, 300, 400, 500, 600)
     var i = 0
 
     for (i in 0..numeros.size - 1) {
         println(numeros[i])
+    }
+
+    anotherWay1()
+
+}
+
+fun anotherWay1 () {
+
+    for (i in 1..10) {
+        println("$i ")
+    }
+
+    for (j in 0..20 step 2) {
+        println("$j ")
+    }
+
+    for (k in 20 downTo 0 step 5) {
+        println("$k ")
+    }
+
+    for (l in 10 downTo 0) {
+        println("$l ")
+    }
+
+    val string = "Danilo Volles Araujo"
+    for (char in string) {
+        print("$char")
     }
 }
