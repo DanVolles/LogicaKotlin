c72cd59bea4e9cb6ba59c1753fccd4f39fb8e0bd
Danilo Volles <danilo.volles@outlook.com>
2023-02-22 11:46:59 -0300

    Elvis operator example.

---
+++ src/main/kotlin/study/exceptions/elvisOperator.kt
@@ -0,0 +1,21 @@
+package study.exceptions
+
+/*
+O operador Elvis é um tipo de controle de fluxo que necessita o entendimento de exceptions e Null Safety
+verifica o que temos à esquerda e caso seja nulo, executamos o que temos à direita **1
+ele substitui o controle de fluxo if determinado abaixo
+ */
+
+fun main() {
+
+    val str: String? = null
+
+    if (str == null) {
+        println("null")
+    } else {
+        println(str)
+    }
+
+    println(str ?: "null") // **1
+
+}
