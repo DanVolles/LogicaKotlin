f09c8390a1e3a1b498c508e0e8eda84c1751483f
Danilo Volles <danilo.volles@outlook.com>
2023-03-03 22:13:08 -0300

    pow() math function reminder.

--- src/main/kotlin/study/theory/functions/mathFunctions.kt
+++ src/main/kotlin/study/theory/functions/mathFunctions.kt
@@ -6,7 +6,10 @@ fun main() {
 
     println("O maior número é: " + max(5, 6))
     println("O menor número é: " + min(5, 6))
+
+    // Tanto a raíz quadrada quanto o quadrado de um número necessitam de Doubles para aplicar a função
     println("A raiz quadrada de 25 é: " + sqrt(25.0))
+    println("O quadrado de 5 é: ${5.00.pow(2)}.")
 
     println(PI) // pi
     println(E) // Euler
