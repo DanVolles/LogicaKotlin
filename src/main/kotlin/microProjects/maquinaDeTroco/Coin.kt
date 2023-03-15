367df5f23ceb3a8b2bc04f8b5914b5f28544849f
Danilo Volles <danilo.volles@outlook.com>
2023-03-15 18:13:55 -0300

    Criação da classe Coin e da main function

---
+++ src/main/kotlin/microProjects/maquinaDeTroco/Coin.kt
@@ -0,0 +1,13 @@
+package microProjects.maquinaDeTroco
+
+class Coin (private val value: Double) {
+
+    fun coinIn(): Boolean {
+        return true
+    }
+
+    fun coinOut(): Boolean {
+        return true
+    }
+
+}
