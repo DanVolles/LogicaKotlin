4d3621c288d089d0a3cd5a98d06b1d7098064e8f
Danilo Volles <danilo.volles@outlook.com>
2023-05-30 10:07:44 -0300

    Modificadores de visibilidade, override e overload.

---
+++ src/main/kotlin/study/theory/oop/heranca/heranca.kt
@@ -0,0 +1,36 @@
+package study.theory.oop.heranca
+
+open class Eletronico(var marca: String) {
+    private fun ativarCorrente() {}
+    // Pode ser privado, por que não precisa ser acessado pelo computador
+    fun ligar(){
+        ativarCorrente()
+    }
+    fun desligar(){}
+}
+
+// Estou dizendo que o computador é um eletrônico
+class Computador(marca: String): Eletronico (marca) {
+    fun processar(){}
+    fun instalarSoftware(){}
+}
+
+/*
+Evite repetir códigos que fazem a mesma coisa
+duplicar código é manutenção em dobro, problema em dobro
+NÃO REPITA CÓDIGO
+ */
+fun main() {
+    val computador = Computador("DELL")
+    var geladeira = Eletronico("Brastemp")
+
+    // Os métodos ligar() e desligar(), mesmo que não estejam definidos no escopo de "Computador"
+    // Podem ser acessados pelo objeto "c"
+    with(computador) {
+        ligar()
+        desligar()
+        processar() // você não pode fazer um "g.processar()"
+        instalarSoftware() // O mesmo vale para "g.instalarSoftware()"
+        // ativarCorrente() // Não pode ser utilizado
+    }
+}
---
+++ src/main/kotlin/study/theory/oop/heranca/overrideOverload.kt
@@ -0,0 +1,37 @@
+package study.theory.oop.heranca
+
+open class Eletronico2 (var marca: String){
+    private fun corrente(ativo: Boolean){}
+    fun ligar() {
+        corrente(true)
+    }
+    open fun desligar(){
+        // Essa função precisa ser open,
+        corrente(false)
+    }
+}
+
+class Computador2 (marca: String): Eletronico2(marca) {
+//    fun desligarComputador(){ // Fica confuso,não é uma boa
+//        save()
+//        desligar()
+//    }
+    override fun desligar(){
+        save()
+        super.desligar() // Esse desligar é o desligar do Eletrônico
+    }
+
+    fun save() {}
+    fun save(a: Int){} // É possível, já que as assinaturas estão diferentes
+    fun save(b: Boolean){} // Idem
+
+}
+
+fun main() {
+    val c: Computador2 = Computador2("DELL")
+
+    with(c) {
+        this.ligar()
+        this.desligar()
+    }
+}
